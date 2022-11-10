e4edd7508906f2610a19545c63fee5e32c63bcfe
hidharm <hidharm@gmail.com>
2022-11-10 09:21:57 +0530

    firstupdate

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,1 +1,9 @@
 print("hello universe")
+
+
+
+# git clone --bare https://github.com/exampleuser/public-repo.git
+# cd public-repo.git
+# git push --mirror https://github.com/yourname/private-repo.git
+# cd ..
+# rm -rf public-repo.git
